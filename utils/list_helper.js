b365a6475f70a2c8749ca8ce07d1f96a56e654af
AlekseyVY <alekseyVY@gmail.com>
2020-05-18 17:45:14 +0400

    exercises 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,8 +8,22 @@ const totalLikes = (blogs) => {
     return tmp
 }
 
+const favoriteBlog = (blogs) => {
+    let tmpLikes = 0
+    let tmpBlogObject = null
+
+    blogs.map(blog => {
+        if(blog.likes > tmpLikes){
+            tmpLikes = blog.likes
+            tmpBlogObject = blog
+        }
+    })
+    return tmpBlogObject
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
 
