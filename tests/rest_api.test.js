d423f7ff65ff6fdd7cda7814c2564e529a1bb3b8
AlekseyVY <alekseyVY@gmail.com>
2020-05-19 21:16:35 +0400

    exercises 4.9

--- tests/rest_api.test.js
+++ tests/rest_api.test.js
@@ -20,8 +20,13 @@ describe('RESTful API testing block: ', () => {
     test('GET on api/blogs returns all blogs', async () => {
     const response = await api.get('/api/blogs')
         .expect(200)
-        .expect('Content-Type', /application\/json/)
     expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
+
+    test('check that returned blog have id property', async () => {
+        const result = await api.get('/api/blogs')
+            .expect(200)
+        expect(result.body[0].id).toBeDefined()
     })
 
 })
